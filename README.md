e2f1c29fb07499479843c910aaa7fdf2bdef3f15
Milena du Manoir <milenadumanoir@gmail.com>
2021-09-19 21:28:27 +0200

    Comment and clean the code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Exercise 2 for *lelivrescolaire*
+
+Card Slider with 2 buttons allowing to slide right or left, and a button activating autoslide.
--- main.js
+++ main.js
@@ -1,79 +1,79 @@
+//Function allows to slide to the next card on the right
 function slideRight()  {
-    // si center n'a pas de next ele fais rien
+    //If center element has next element...
     var centerElement = document.querySelector(".center");
     if (centerElement.nextElementSibling) {
-    //change la classe de ele2 en mini : 
-        //attrape ele central dans dom, enelve classe center, donne classe mini
+    
+        //Change the class of center element to mini
         centerElement = document.querySelector(".center");
         centerElement.classList.remove("center");
         centerElement.classList.add("mini");
 
-    //supprime la classe de ele précedent 
+        //Remove the mini class of previous element, if it exists 
         var miniElement = centerElement.previousElementSibling;
-       if (centerElement.previousElementSibling) {miniElement.classList.remove("mini");}
-      
-
-    //change la classe de l'ele suivant
+        if (centerElement.previousElementSibling) {miniElement.classList.remove("mini");}
+        
+        //Change class of next element from mini to center
         miniElement = centerElement.nextElementSibling;
         miniElement.classList.remove("mini");
         miniElement.classList.add("center");
-    //change la classe de l'ele d(encore après
+        
+        //Add mini class to element after next
         miniElement = miniElement.nextElementSibling;
         miniElement.classList.add("mini");
      }
-
-    //change la classe de ele4 en mini
-    //enlever la classe des elements à ne pas afficher
 }
-
+//Function allows to slide to the next card on the left
 function slideLeft() {
-    // si center n'a pas de next ele fais rien
+    // If center element has previous element...
     var centerElement = document.querySelector(".center");
     if (centerElement.previousElementSibling) {
-    //change la classe de ele2 en mini : 
-        //attrape ele central dans dom, enelve classe center, donne classe mini
+    
+        //Change the class of center element to mini
         var centerElement = document.querySelector(".center");
         centerElement.classList.remove("center");
         centerElement.classList.add("mini");
 
-    //supprime la classe de ele précedent 
+        //Remove the mini class of next element, if it exists 
         var miniElement = centerElement.nextElementSibling;
         if (centerElement.nextElementSibling) {miniElement.classList.remove("mini");}
 
-    //change la classe de l'ele suivant
+        //Change class of previous element from mini to center
         miniElement = centerElement.previousElementSibling;
         miniElement.classList.remove("mini");
         miniElement.classList.add("center");
-    //change la classe de l'ele d(encore après
+
+        //Add mini class to element before previous
         miniElement = miniElement.previousElementSibling;
         miniElement.classList.add("mini");
-        
-
-    //change la classe de ele4 en mini
-    //enlever la classe des elements à ne pas afficher
+    
     }
 
 }
-
+//Remember the state of autoslide
 var autoEnabled = false
-var myVar 
+//Timer interval to enable stoping of auto
+var interTimer
+
+//Function allows to slide every 3s when enabled
 function autoSlide() {
 
     if (!autoEnabled)  {
-        myVar = setInterval(slideIfNotLast,  3000);
+        interTimer = setInterval(slideIfNotLast,  3000);
         autoEnabled = true;
         var button = document.querySelector(".button");
         button.classList.remove("off");
         button.classList.add("on");
     }
     else {
-        clearInterval(myVar);
+        clearInterval(interTimer);
         autoEnabled = false;
         var button = document.querySelector(".button");
         button.classList.remove("on");
         button.classList.add("off");
     }
 }
+//Function checks if we have reached a boundary
 function slideIfNotLast() {
     var centerElement = document.querySelector(".center");
     if (centerElement.nextElementSibling) {slideRight();
